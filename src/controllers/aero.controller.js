8ab6dd7b731341cd6917365025ba87de7bc2ca62
stefanristic40 <stefanristic407@gmail.com>
2024-06-15 01:20:41 -0700

    refactor: Update Aero API URLs to use constant variable

--- src/controllers/aero.controller.js
+++ src/controllers/aero.controller.js
@@ -4,6 +4,8 @@ const { default: axios } = require('axios');
 const catchAsync = require('../utils/catchAsync');
 const config = require('../config/config');
 
+const AERO_API_URL = 'https://aeroapi.flightaware.com/aeroapi';
+
 const fetchAllFlights = async (url, headers) => {
   let allFlights = [];
   let nextUrl = url;
@@ -14,19 +16,17 @@ const fetchAllFlights = async (url, headers) => {
     const { flights, links } = response.data;
 
     allFlights = allFlights.concat(flights);
-    nextUrl = links && links.next ? `https://aeroapi.flightaware.com/aeroapi${links.next}` : null;
+    nextUrl = links && links.next ? `${AERO_API_URL}${links.next}` : null;
   }
 
   return allFlights;
 };
 
 const searchFlights = catchAsync(async (req, res) => {
-  //   28.152890667136678, -82.55100763322196
-  // 28.105578910933378, -82.47799101938415
-  const initialUrl =
-    'https://aeroapi.flightaware.com/aeroapi/flights/search?query=-latlong+%2244.953469+-111.045360+40.962321+-104.046577%22';
-  // const initialUrl =
-  //   'https://aeroapi.flightaware.com/aeroapi/flights/search?query=-latlong+%2228.152890667136678+-82.55100763322196+28.105578910933378+-82.47799101938415%22';
+  // Get query params
+  const { lat1, lon1, lat2, lon2 } = req.query;
+  const initialUrl = `${AERO_API_URL}/flights/search?query=-latlong+%22${lat1}+${lon1}+${lat2}+${lon2}%22`;
+
   const headers = {
     Accept: 'application/json; charset=UTF-8',
     'x-apikey': config.aero_api_key,
@@ -46,7 +46,7 @@ const getFlightTrack = catchAsync(async (req, res) => {
       'x-apikey': config.aero_api_key,
     };
 
-    const response = await axios.get(`https://aeroapi.flightaware.com/aeroapi/flights/${id}/track`, { headers });
+    const response = await axios.get(`${AERO_API_URL}/flights/${id}/track`, { headers });
 
     res.status(httpStatus.OK).send(response.data);
   } catch (error) {
